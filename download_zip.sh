7cf78d53e4ed52807d5828c79e93bd20db0eafa0
leejtding <lee.jt.ding@gmail.com>
2020-11-22 18:21:08 -0500

    Updated data download script

--- download_zip.sh
+++ download_zip.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+#this creates data directory where the data zip file will be downloaded to
 mkdir data
 cd data
 
+#this downloads the zip file that contains the data
 curl https://zenodo.org/record/2538136/files/hirise-map-proj-v3.zip?download=1 --output deimos.zip
 
 cd ..
