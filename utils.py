a5cbbd61c0f485bdb2325c9238b92206f313a474
George Hu <george_hu@brown.edu>
2020-11-22 23:06:00 -0500

    Start utils.py, modify gitignore

---
+++ utils.py
@@ -0,0 +1,14 @@
+
+
+'''
+Function to return dictionary that maps [image name] to class (int)
+'''
+def get_class_dict(class_label_path):
+    class_dict = {}
+    with open(class_label_path, 'r') as f:
+        for ln in f:
+            splts = ln.split(' ')
+            class_dict[splts[0]] = int(splts[1])
+
+    return class_dict
+
